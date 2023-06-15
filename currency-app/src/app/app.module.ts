126693fdf3c9150e9cdfce11398f29ef72a26589
Anna Berezhna <anna.berejna@gmail.com>
2023-06-15 21:06:26 +0300

    currency converter component

--- currency-app/src/app/app.module.ts
+++ currency-app/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrencyComponent } from './currency/currency.component';
+import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
 
 @NgModule({
-  declarations: [AppComponent, CurrencyComponent],
+  declarations: [AppComponent, CurrencyComponent, CurrencyConverterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
