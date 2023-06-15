d3f9e14ec1fa374b01af0b35fb149de470328230
Anna Berezhna <anna.berejna@gmail.com>
2023-06-15 15:49:39 +0300

    http module

--- currency-app/src/app/app.module.ts
+++ currency-app/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrencyComponent } from './currency/currency.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CurrencyComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CurrencyComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
