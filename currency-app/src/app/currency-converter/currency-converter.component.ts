80dbcad4349e5fb20fc2f73f9bee38d607e8ab32
Anna Berezhna <anna.berejna@gmail.com>
2023-06-17 00:35:51 +0300

    component logic

--- currency-app/src/app/currency-converter/currency-converter.component.ts
+++ currency-app/src/app/currency-converter/currency-converter.component.ts
@@ -7,29 +7,63 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./currency-converter.component.css'],
 })
 export class CurrencyConverterComponent implements OnInit {
-  amount: number = 0; // Значення кількості одиниц для конвертації
-  currency1: string = 'UAH'; // Перша валюта
-  currency2: string = 'USD'; // Друга валюта
-  conversionRates: any; // Об'єкт з курсами обміну валют
-  convertedAmount: number = 0; // Конвертована сума
+  amount1: number = 0;
+  amount2: number = 0;
+  currency1: string = 'UAH';
+  currency2: string = 'USD';
+  conversionRates: any = {};
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.fetchConversionRates(); // Отримати курси обміну при завантаженні компонента
+    this.getConversionRates();
   }
 
-  fetchConversionRates() {
-    this.http
-      .get<any>('https://open.er-api.com/v6/latest/UAH') // Виконати GET-запит до зовнішнього API
-      .subscribe((data) => {
-        this.conversionRates = data; // Зберегти отримані курси обміну
-        this.convertCurrency(); // Викликати функцію конвертації
-      });
+  // Отримання курсів конвертації при ініціалізації компонента
+  getConversionRates() {
+    const url = 'https://open.er-api.com/v6/latest/UAH';
+    this.http.get(url).subscribe(
+      (data: any) => {
+        this.conversionRates = data.rates;
+        this.convertCurrencies1(); // Перерахунок значення amount2
+        this.convertCurrencies2(); // Перерахунок значення amount1
+      },
+      (error: any) => {
+        console.log('Failed to fetch conversion rates:', error);
+      }
+    );
   }
 
-  convertCurrency() {
-    const rate = this.conversionRates[this.currency1][this.currency2]; // Отримати курс обміну між валютами
-    this.convertedAmount = this.amount * rate; // Обчислити конвертовану суму
+  // Перерахунок значення amount2 на основі amount1
+  convertCurrencies1() {
+    const rate1 = this.conversionRates[this.currency1];
+    const rate2 = this.conversionRates[this.currency2];
+
+    if (rate1 && rate2) {
+      this.amount2 = Number(((this.amount1 / rate1) * rate2).toFixed(2));
+    } else {
+      console.log('Invalid conversion rates');
+    }
+  }
+
+  // Перерахунок значення amount1 на основі amount2
+  convertCurrencies2() {
+    const rate1 = this.conversionRates[this.currency1];
+    const rate2 = this.conversionRates[this.currency2];
+
+    if (rate1 && rate2) {
+      this.amount1 = Number(((this.amount2 / rate2) * rate1).toFixed(2));
+    } else {
+      console.log('Invalid conversion rates');
+    }
+  }
+
+  // Обмін валют
+  swapCurrencies() {
+    const tempCurrency = this.currency1;
+    this.currency1 = this.currency2;
+    this.currency2 = tempCurrency;
+    this.convertCurrencies1(); // Перерахунок значення amount2
+    this.convertCurrencies2(); // Перерахунок значення amount1
   }
 }
